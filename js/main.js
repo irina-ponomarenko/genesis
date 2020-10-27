c2ce918a9a4ba0f688f82cf00f9c5a6b6c7eb3b7
irinaponomarenko <ponomarenko02041993@gmail.com>
2020-10-27 21:38:51 +0200

    next block

--- js/main.js
+++ js/main.js
@@ -1,43 +1,50 @@
 $(document).ready(function(){
-    $(window).resize(function() {   //скрипт скрола по странице
-        width = $(window).width();
-        if (width >= 1110) {
-            function Scroll_block(){
-                let scroll_top = $(document).scrollTop();
-                $(".menu a").each(function(){
-                    let hash = $(this).attr("href");
-                    let target = $(hash);
-                    if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
-                        $('.link-menu').removeClass('active-link');
-                        $(this).closest('.item-menu').find('.link-menu').addClass('active-link');
-                    } else {
-                        $(this).closest('.item-menu').find('.link-menu').removeClass('active-link');
-                    }
-                });
-            }
-
-
-            $(document).on("scroll", Scroll_block);
-
-            $("a[href^='#']").click(function(e){
-                e.preventDefault();
-
-                $(document).off("scroll");
+    function Scroll_block(){
+        let scroll_top = $(document).scrollTop();
+        $(".menu a").each(function(){
+            let hash = $(this).attr("href");
+            let target = $(hash);
+            if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
                 $('.link-menu').removeClass('active-link');
                 $(this).closest('.item-menu').find('.link-menu').addClass('active-link');
-                let hash = $(this).attr("href");
-                let target = $(hash);
+            } else {
+                $(this).closest('.item-menu').find('.link-menu').removeClass('active-link');
+            }
+        });
+    }
 
-                $("html, body").animate({
-                    scrollTop: target.offset().top,
-                }, 500, function(){
-                    window.location.hash = hash;
-                    $(document).on("scroll", Scroll_block);
-                });
 
-            });
-        }
+    $(document).on("scroll", Scroll_block);
+
+    $("a[href^='#']").click(function(e){
+        e.preventDefault();
+
+        $(document).off("scroll");
+        $('.link-menu').removeClass('active-link');
+        $(this).closest('.item-menu').find('.link-menu').addClass('active-link');
+        let hash = $(this).attr("href");
+        let target = $(hash);
+
+        $("html, body").animate({
+            scrollTop: target.offset().top,
+        }, 500, function(){
+            window.location.hash = hash;
+            $(document).on("scroll", Scroll_block);
+        });
+
     });
+
+    $('.menu-btn').on('click', function () {
+        $(this).closest('.menu-top').find('.mobile-menu').addClass('active-menu');
+        $(this).closest('.menu-top').find('.container-menu-btn').addClass('toggle-btn');
+    });
+
+    $('.close-menu').on('click', function () {
+        $(this).closest('.menu-top').find('.mobile-menu').removeClass('active-menu');
+        $(this).closest('.menu-top').find('.container-menu-btn').removeClass('toggle-btn');
+
+    });
+
 
 });
 
