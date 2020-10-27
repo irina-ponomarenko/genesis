7d437119385f36ff95c63c3236d0e53e5f774d13
irinaponomarenko <ponomarenko02041993@gmail.com>
2020-10-27 10:16:21 +0200

    next block

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    function Scroll_block(){
+        let scroll_top = $(document).scrollTop();
+        $(".menu a").each(function(){
+            let hash = $(this).attr("href");
+            let target = $(hash);
+            if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+                $('.link-menu').removeClass('active-link');
+                $(this).closest('.item-menu').find('.link-menu').addClass('active-link');
+            } else {
+                $(this).closest('.item-menu').find('.link-menu').removeClass('active-link');
+            }
+        });
+    }
+
+
+    $(document).on("scroll", Scroll_block);
+
+    $("a[href^='#']").click(function(e){
+        e.preventDefault();
+
+        $(document).off("scroll");
+        $('.link-menu').removeClass('active-link');
+        $(this).closest('.item-menu').find('.link-menu').addClass('active-link');
+        let hash = $(this).attr("href");
+        let target = $(hash);
+
+        $("html, body").animate({
+            scrollTop: target.offset().top,
+        }, 500, function(){
+            window.location.hash = hash;
+            $(document).on("scroll", Scroll_block);
+        });
+
+    });
+});
+
